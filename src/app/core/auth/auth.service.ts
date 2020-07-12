d21ed9b6d066e6615158e79c434acd62b8e7bc3f
williamgundim <williammatos09@gmail.com>
2020-07-12 14:52:38 -0300

    colocado tap do rxjs

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 const API_URL = 'http://localhost:3000'
 
@@ -13,15 +14,23 @@ export class AuthService {
 
   }
 
-
+  /**authenticate
+   * @description Método de autenticação dos usuários.
+   * @param userName - user 
+   * @param password - senha do usuário 
+   */
   authenticate(userName:string, password: string){
 
     //nome da propriedade tem o mesmo nome da variavel, pode passar de forma simplificada.
     //let oUser:object = {userName, password}
     let oUser: Object = {userName: userName, password: password}
 
-    return this.http.post(API_URL + '/user/login', oUser)
-
+    return this.http
+      .post(API_URL + '/user/login', oUser, {observe: 'response'} )
+      .pipe(tap( res =>{
+        //utiliza o tap para armanezar o token, ele é chamado entre a resposta e o subscribe.  
+        const authToken = res.headers.get('x-access-token');
+      }))
   }
 
 }
